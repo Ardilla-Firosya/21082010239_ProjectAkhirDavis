729b68cb237e4dd60d1f0ad0cd422878f5605dc0
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-06-21 00:46:32 +0700

    Update UASDAVIS.py

--- UASDAVIS.py
+++ UASDAVIS.py
@@ -282,8 +282,6 @@ def main():
     Data Visualisasi tersebut menggunakan Histogram Column Chart untuk menampilkan distribusi jumlah penjualan tiap bulan. Komponen grafik ini terdiri dari label sumbu x yaitu Month (Bulan) dan label sumbu y yaitu Total Sales Amount (Jumlah penjualan). Berdasarkan hasil tersebut, diketahui bahwa jumlah penjualan perbulan tertinggi terletak pada bulan ke-10 (Oktober) sekitar 1,640,296.00 dan jumlah penjualan terendah terletak pada bulan ke-11 (November) sekitar 45,642.00.
     </div>
     """, unsafe_allow_html=True)
-
-    st.markdown("<hr>", unsafe_allow_html=True)  # Garis horizontal
         
     elif dataset_choice == "IMDB Movies":
         st.markdown("<h2 style='text-align: center;'>Dataset IMDB Movies🎬</h2>", unsafe_allow_html=True)
