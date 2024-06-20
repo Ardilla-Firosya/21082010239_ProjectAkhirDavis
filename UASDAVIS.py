392131c7fb4671b2fea678ba0f1d5c8d8e102932
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-06-21 00:56:19 +0700

    Update UASDAVIS.py

--- UASDAVIS.py
+++ UASDAVIS.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import numpy as np
 from sqlalchemy import create_engine
 import pymysql
-import random
 
 # Fungsi untuk membuat koneksi ke database AdventureWorks
 def create_connection_aw():
@@ -68,7 +66,7 @@ def main():
         st.write("Comparison: Bar Chart")
         
         # SQLAlchemy connection and query for bar chart
-    try:
+        try:
             # Establish the database connection using SQLAlchemy
             engine = create_engine("mysql+pymysql://davis2024irwan:wh451n9m%40ch1n3@kubela.id:3306/aw")
     
@@ -96,8 +94,8 @@ def main():
             top_n = 7
             top_categories = df_viz.nlargest(top_n, 'SalesAmount')
             others = pd.DataFrame({
-            'ProductSubCategory': ['Others'],
-            'SalesAmount': [df_viz['SalesAmount'][~df_viz['ProductSubCategory'].isin(top_categories['ProductSubCategory'])].sum()]
+                'ProductSubCategory': ['Others'],
+                'SalesAmount': [df_viz['SalesAmount'][~df_viz['ProductSubCategory'].isin(top_categories['ProductSubCategory'])].sum()]
             })
         
             # Combine the top categories with the 'Others' category
@@ -111,32 +109,30 @@ def main():
             plt.ylabel('Product Sub Category')
             plt.tight_layout()
     
-            
             # Display the plot in Streamlit
             st.pyplot(plt)
             
-    except Exception as e:
-        st.error(f"Error: {e}")
+            st.markdown("""
+            <div style='text-align: justify;'>
+            <b>Deskripsi Data Visualisasi:</b> <br>
+            Visualisasi data tersebut menggunakan Grafik Bar Chart dengan menampilkan jumlah penjualan berdasarkan sub category product dari data AdventureWorks. Grafik tersebut terdiri antara sumbu x sebagai 'Sales Amount' (jumlah penjualan) dan sumbu y sebagai 'Product Sub Category'. Terdapat 8 subcategory dengan penjualan tertinggi dan kategori others (kategori produk lain) dimana jumlah penjualan digabungkan menjadi satu kategori yang memiliki jumlah penjualan paling sedikit.
+            </div>
+            """, unsafe_allow_html=True)
     
-        st.markdown("""
-        <div style='text-align: justify;'>
-        <b>Deskripsi Data Visualisasi:</b> <br>
-        Visualisasi data tersebut menggunakan Grafik Bar Chart dengan menampilkan jumlah penjualan berdasarkan sub category product dari data AdventureWorks. Grafik tersebut terdiri antara sumbu x sebagai 'Sales Amount' (jumlah penjualan) dan sumbu y sebagai 'Product Sub Category'. Terdapat 8 subcategory dengan penjualan tertinggi dan kategori others (kategori produk lain) dimana jumlah penjualan digabungkan menjadi satu kategori yang memiliki jumlah penjualan paling sedikit.
-        </div>
-        """, unsafe_allow_html=True)
-    
-        st.markdown("""
-        <div style='text-align: justify;'>
-        <ul>
-        <li>Road Bikes memiliki jumlah penjualan tertinggi yaitu sekitar 14 juta</li>
-        <li>Montain Bikes memiliki jumlah penjualan tertinggi ke dua sekitar 9 juta</li>
-        <li>Touring Bikes memiliki jumlah penjualan sekitar 5 juta</li>
-        <li>Tires dan Tubes memiliki jumlah penjualan sekitar 1 juta</li>
-        <li>Dan terakhir Kategori others memiliki jumlah penjualan lebih dari 100 ribu</li>
-        </ul>
-        </div>
-        """, unsafe_allow_html=True)
+            st.markdown("""
+            <div style='text-align: justify;'>
+            <ul>
+            <li>Road Bikes memiliki jumlah penjualan tertinggi yaitu sekitar 14 juta</li>
+            <li>Montain Bikes memiliki jumlah penjualan tertinggi ke dua sekitar 9 juta</li>
+            <li>Touring Bikes memiliki jumlah penjualan sekitar 5 juta</li>
+            <li>Tires dan Tubes memiliki jumlah penjualan sekitar 1 juta</li>
+            <li>Dan terakhir Kategori others memiliki jumlah penjualan lebih dari 100 ribu</li>
+            </ul>
+            </div>
+            """, unsafe_allow_html=True)
 
+        except Exception as e:
+            st.error(f"Error: {e}")
         
     elif dataset_choice == "IMDB Movies":
         st.markdown("<h2 style='text-align: center;'>Dataset IMDB Movies🎬</h2>", unsafe_allow_html=True)
