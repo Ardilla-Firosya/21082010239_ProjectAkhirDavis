aad4d25c0383d0759b7bd868ebc560936f3aedc5
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-06-21 01:21:55 +0700

    Update UASDAVIS.py

--- UASDAVIS.py
+++ UASDAVIS.py
@@ -53,6 +53,8 @@ def load_data_imdb(file_path):
 
 # Main function to set up the Streamlit app
 def main():
+    # Judul utama aplikasi
+    st.markdown("<h1 style='text-align: center;'>Data Visualization</h1>", unsafe_allow_html=True)
     
     # Sidebar untuk memilih dataset
     dataset_choice = st.sidebar.radio("Pilih Dataset", ("Adventure Works", "IMDb top Movies"))
