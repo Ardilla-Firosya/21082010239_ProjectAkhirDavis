2899a513a13b7d518e1a68fe282b67fd38ef542b
Ardilla Firosya <135436843+Ardilla-Firosya@users.noreply.github.com>
2024-06-21 01:24:36 +0700

    Update UASDAVIS.py

--- UASDAVIS.py
+++ UASDAVIS.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
 import seaborn as sns
 from sqlalchemy import create_engine
 import pymysql
